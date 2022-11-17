ad708dbeb61a886d59210549831ab9a3a8006f21
martijor000 <82613327+martijor000@users.noreply.github.com>
2022-11-17 11:23:56 -0800

    Possibly Fixed Ball?

--- WinFormsApp6/Form1.cs
+++ WinFormsApp6/Form1.cs
@@ -5,6 +5,11 @@ namespace WinFormsApp6
         // vector2;
         private int Top;
         private int Left;
+        private int FormHeight;
+        private int FormWidth;
+        private double BallRatio;
+        private Graphics Ball;
+
         public Form1()
         {
             InitializeComponent();
@@ -12,18 +17,25 @@ namespace WinFormsApp6
         private void Form1_Load(object sender, EventArgs e)
         {
             Top = this.Width / 2;
-            Left = this.Height / 2; 
+            Left = this.Height / 2;
+            WindowState = FormWindowState.Maximized;
+            FormHeight = this.Height;
+            FormWidth = this.Width;
         }
+
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.FillEllipse(Brushes.Blue, new Rectangle(Top, Left, 100, 100));
+            BallRatio = FormWidth / (double)FormHeight;
+            g.FillEllipse(Brushes.Blue, new Rectangle(Top, Left, (int)BallRatio * 100, (int)BallRatio * 100));
+            Ball = g;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Left += 1;
             Top += 1;
+           
             Invalidate();   
 
         }
