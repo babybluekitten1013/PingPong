d6f2694edfb0d8b9d17193674fa03f2b8779694c
martijor000 <82613327+martijor000@users.noreply.github.com>
2022-11-18 00:24:48 -0800

    Updated a lot of stuff

--- WinFormsApp6/Ball.cs
+++ WinFormsApp6/Ball.cs
@@ -8,6 +8,32 @@ namespace STJ_PingPong
 {
     internal class Ball
     {
-        //private void Bal
+        private int Top;
+        private int Left;
+        private double BallRatio;
+        private Graphics BallGraphics;
+
+        public Ball(int FormHeight, int FormWidth)
+        {
+            BallRatio = FormWidth / (double)FormHeight * 100;
+        }
+
+        public void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillEllipse(Brushes.Blue, new Rectangle(Top, Left, (int)BallRatio, (int)BallRatio));
+            BallGraphics = g;
+        }
+        
+        public void ResizeBall(int NewFormHeight, int NewFormWidth)
+        {
+            BallRatio = NewFormWidth / (double)NewFormHeight * 100;
+        }
+
+        public void Move()
+        {
+            Left += 1;
+            Top += 1;
+        }
     }
 }
--- WinFormsApp6/MainForm.cs
+++ WinFormsApp6/MainForm.cs
@@ -1,14 +1,15 @@
+using STJ_PingPong;
+using System.Runtime.CompilerServices;
+
 namespace WinFormsApp6
 {
     public partial class MainForm : Form
     {
         // vector2;
-        private int Top;
-        private int Left;
-        private int FormHeight;
-        private int FormWidth;
-        private double BallRatio;
-        private Graphics Ball;
+
+        //private int FormHeight;
+        //private int FormWidth;
+        private Ball CreateBall;
 
         public MainForm()
         {
@@ -16,40 +17,24 @@ namespace WinFormsApp6
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
+            CreateBall = new Ball(this.Height, this.Width);
             Top = this.Width / 2;
             Left = this.Height / 2;
             WindowState = FormWindowState.Maximized;
-            FormHeight = this.Height;
-            FormWidth = this.Width;
-            BallRatio = FormWidth / (double)FormHeight * 100;
         }
-
         private void MainForm_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            g.FillEllipse(Brushes.Blue, new Rectangle(Top, Left, (int)BallRatio, (int)BallRatio));
-            Ball = g;
+            CreateBall.Ball_Paint(this, e);
+        }
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            CreateBall.ResizeBall(this.Height, this.Width);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            Left += 1;
-            Top += 1;
-           
-            Invalidate();   
-
-        }
-
-        private void MainForm_Resize(object sender, EventArgs e)
-        {
-            ResizeBall();
-        }
-
-        private void ResizeBall()
-        {
-            FormHeight = this.Height;
-            FormWidth = this.Width;
-            BallRatio = FormWidth / (double)FormHeight * 100;
+            CreateBall.Move();
+            Invalidate();
         }
     }
 }
